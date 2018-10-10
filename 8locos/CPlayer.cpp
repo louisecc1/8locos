cc7c1aeaec1077d22fb70e2ccb6358a2a99dc495
Arturo Navarro <arnariv@gmail.com>
2018-10-09 19:59:44 -0500

    Algunso cambios minimos nada importante aun :V

--- 8locos/CPlayer.cpp
+++ 8locos/CPlayer.cpp
@@ -1,7 +1,13 @@
 #include "pch.h"
 #include "CPlayer.h"
+#include <iostream>
 
+using namespace std;
 
 CPlayer::CPlayer(string name)
 	:cp_name(name),score(0),state(false)
 	{}
+
+void CPlayer::verMano() {
+	mano.recorrer();
+}
--- 8locos/CPlayer.h
+++ 8locos/CPlayer.h
@@ -1,16 +1,23 @@
 #pragma once
 #include<iostream>
 #include<string>
+#include "ListaEnlazada.h"
+#include "CMenor.h"
+#include "CNode.h"
 using namespace std;
 class CPlayer
 {
 public:
 	CPlayer() { state = score = 0; }
-	CPlayer(string);	
+	CPlayer(string);
+	void verMano();
+	void ocultarMano();
+	void getNumCartas();
 	
 	string cp_name;
 	int score;
 	bool state;
+	ListaEnlazada<int, char, CMenor<CNode<int, char> > > mano;
 };
 inline bool operator== (const CPlayer & a, const CPlayer &b)
 	{	return a.cp_name == b.cp_name;}
